08cb86ef1b87d46af64fad3ba8b346797bd9806a
Matthew Paff <Matthew.paff@utexas.edu>
2017-03-08 13:38:11 -0600

    Maybe a little messy, but it plots proportion of sims extinct as a function of beta_vax

--- R/disease_incidence_proportion.R
+++ R/disease_incidence_proportion.R
@@ -1,4 +1,3 @@
-load('data/transVax_data/0.00108723582512665_0.0437456717863017_0.2_0.2_5000_1_exp_trans.Rdata')
 library(tidyverse)
 
 get_final_sizes <- function(mat){
@@ -15,26 +14,33 @@ get_final_sizes <- function(mat){
 #   }
 #   return(df)
 # }
-
 get_all_final_sizes <- function(list_mat){
   list_mat %>% map(get_final_sizes) %>%
     bind_rows()
 }
 
+# all_equal(get_all_final_sizes(epidemicRuns), get_all_final_sizes2(epidemicRuns)) ## checks that both outputs are identical
 
-all_equal(get_all_final_sizes(epidemicRuns), get_all_final_sizes2(epidemicRuns))
-
-microbenchmark::microbenchmark(times = 10,
-                               get_all_final_sizes(epidemicRuns),
-                               get_all_final_sizes2(epidemicRuns))
-get_all_final_sizes(epidemicRuns) %>% ggplot(aes(disease))+geom_histogram()
+#microbenchmark::microbenchmark(times = 10,
+#                               get_all_final_sizes(epidemicRuns),
+#                               get_all_final_sizes2(epidemicRuns))
+#get_all_final_sizes(epidemicRuns) %>% ggplot(aes(disease))+geom_histogram()
 
 calc_prop_extinct <- function(df, epi_threshold = 500){
   # Calculates the proportion of simulations that resulted in extinction of the epidemic
   1 - sum(df$disease >= epi_threshold)/nrow(df)
 }
-get_all_final_sizes(epidemicRuns) %>% calc_prop_extinct()
+# get_all_final_sizes(epidemicRuns) %>% calc_prop_extinct()
 
-list.files()
+prop_extinct <- function(file){
+  # load file and return data frame with beta_vax and calculation for proportion of simulations ending in extinction
+  load(file)
+  data.frame(beta=params['beta_vax'], proportion=na.omit(get_all_final_sizes(epidemicRuns)) %>% calc_prop_extinct())
+}
 
+files = list.files('data/transVax_data/', pattern='.Rdata', full.names=TRUE)
+
+props <- lapply(files, 'prop_extinct') %>% bind_rows()
+
+props %>% ggplot(aes(x=beta_vax, y=proportion)) + geom_point()
 
